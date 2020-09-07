47e5d5f90e4177aa678b27c6cc1968bd17e65d12
Ghevi <ghevi96@gmail.com>
2020-09-07 23:10:35 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import { RecipeList } from './recipe-list/recipe-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-list/recipe-detail/recipe-detail.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeList,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
